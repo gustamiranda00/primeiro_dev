b222c32fc027b63b6717eeab94e13ffd197bad21
gustamiranda00 <gustavomiranda@colegiopaulodetarso.com.br>
2025-09-04 00:29:43 +0000

    git add

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("toggleTheme");
-  btn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    btn.textContent = document.body.classList.contains("dark-mode") ? "☀️ Alternar Tema" : "🌙 Alternar Tema";
-  });
-});
